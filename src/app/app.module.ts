00b854e65c7b9c2b836d3ab2695df35b8cc459e4
Debarshi Bhattacharjee <debarshib80@gmail.com>
2019-08-30 19:26:22 +0530

    components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostsDetailComponent } from './components/posts-detail/posts-detail.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SigninComponent,
+    DashboardComponent,
+    PostsComponent,
+    PostsDetailComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
