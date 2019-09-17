98e2dac16f45a5691985120e33d3ef5cb9dedffa
Debarshi Bhattacharjee <debarshib80@gmail.com>
2019-09-17 15:23:02 +0530

    navigate to dashboard on successful login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { LoginComponent } from "./components/login/login.component";
 import { SignUpComponent } from "./components/signup/signup.component";
 import { PostsComponent } from "./components/posts/posts.component";
 import { HomeComponent } from "./components/home/home.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignUpComponent },
-  { path: "posts", component: PostsComponent }
+  { path: "posts", component: PostsComponent },
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  }
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
 import { AuthService } from "src/app/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -8,7 +9,7 @@ import { AuthService } from "src/app/auth.service";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     console.log(this.loginForm);
@@ -20,16 +21,29 @@ export class LoginComponent implements OnInit {
 
   login(user: FormGroup): void {
     this.authService.login(user.value).subscribe(
-      res => console.log(res),
+      res => {
+        console.log(res);
+        localStorage.setItem("token", res.token);
+        this.router.navigate(["/dashboard"]);
+      },
       err => {
         console.log(err);
-        this.loginForm.setErrors({ servererror: err.error.message });
-        this.loginForm
-          .get("email")
-          .setErrors({ servererror: err.error.error.email });
-        this.loginForm
-          .get("password")
-          .setErrors({ servererror: err.error.error.password });
+        if (err.error.message) {
+          this.loginForm.setErrors({ servererror: err.error.message });
+        } else {
+          this.loginForm.setErrors({ servererror: null });
+        }
+        if (err.error.error) {
+          this.loginForm
+            .get("email")
+            .setErrors({ servererror: err.error.error.email });
+          this.loginForm
+            .get("password")
+            .setErrors({ servererror: err.error.error.password });
+        } else {
+          this.loginForm.get("password").setErrors({ servererror: null });
+          this.loginForm.get("email").setErrors({ servererror: null });
+        }
       }
     );
     console.log(this.loginForm);
